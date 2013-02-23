18250c374c68ae581d709ec99c3ec2a96ca8d7ca
waylybaye <havelove@gmail.com>
2013-02-23 22:25:30 +0800

    Support mailbox use SMTP send mail

--- mailman/mail/tasks.py
+++ mailman/mail/tasks.py
@@ -1,6 +1,6 @@
 from celery import task
 from django.core.mail import EmailMultiAlternatives, get_connection
-from mailman.mail.models import Message
+from mailman.mail.models import Message, MailBoxType
 
 
 @task()
@@ -22,7 +22,25 @@ def send_message(message_id):
         msg.attach_alternative(message.html_content, "text/html")
 
     #todo: use different backend for different mailbox according config
-    connection = get_connection()
+    mailbox = message.mailbox
+
+    if mailbox.type == MailBoxType.SMTP:
+        backend = "django.core.mail.backends.smtp.EmailBackend"
+        connection = get_connection(
+            backend=backend,
+            host=mailbox.smtp_host,
+            port=int(mailbox.smtp_port),
+            username=mailbox.smtp_username,
+            password=mailbox.smtp_password,
+            use_tls=mailbox.smtp_use_tls
+        )
+    elif mailbox.type == MailBoxType.SES:
+        backend = "django_ses.SESBackend"
+        connection = get_connection(backend )
+    else:
+        return
+
+    print "Backend: ", backend
     msg.connection = connection
 
     msg.send(fail_silently=False)
