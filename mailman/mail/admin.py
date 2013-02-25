72fbf294e85643c1fc4dac26f676cf73ef471e67
waylybaye <havelove@gmail.com>
2013-02-25 18:01:26 +0800

    update mail admin list_display

--- mailman/mail/admin.py
+++ mailman/mail/admin.py
@@ -2,5 +2,11 @@ from django.contrib import admin
 from mailman.mail.models import MailBox, Message
 
 
-admin.site.register(MailBox)
-admin.site.register(Message)
+class MailBoxAdmin(admin.ModelAdmin):
+    list_display = ["user", "domain", "type"]
+
+class MessageAdmin(admin.ModelAdmin):
+    list_display = ["subject", "mailbox", "is_sent", "sent_at"]
+
+admin.site.register(MailBox, MailBoxAdmin)
+admin.site.register(Message, MessageAdmin)
